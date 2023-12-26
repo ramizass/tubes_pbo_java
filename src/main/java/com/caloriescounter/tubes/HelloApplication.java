2854cc1e691d6e4572d4613cd0ad35421eca8cbd
ramizass <88464165+ramizass@users.noreply.github.com>
2023-12-26 23:51:22 +0700

    Update HelloApplication.java

--- src/main/java/com/caloriescounter/tubes/HelloApplication.java
+++ src/main/java/com/caloriescounter/tubes/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.f xml"));
-        Scene scene = new Scene(fxmlLoader.load(getClass().getResource("hello-view.fxml")), 320, 600);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(getClass().getResource("hello-view.fxml")), 320, 500);
         stage.setTitle("Calories Counter");
         stage.setScene(scene);
         stage.show();
